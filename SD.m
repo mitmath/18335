5a4ad36f3e93270ef08f75b05c8b6e45adf41e54
Steven G. Johnson <stevenj@mit.edu>
2019-01-30 11:34:35 -0500

    fall 2011

--- SD.m
+++ SD.m
@@ -1,20 +1,14 @@
-% Usage: [x, residnorm, residnorm2] = SD(A, b, x, nmax)
+% Usage: x = SD(A, b, x, nmax)
 %
 % Performs nmax steps of steepest descent to solve Ax = b for x,
 % given a starting guess x (e.g. a random vector).  A should be
 % Hermitian positive-definite.  Returns the improved solution x.
-%
-% residnorm is an array of length nmax of the residuals |r| as
-% computed during the SD iterations.  residnorm2 is the same thing,
-% but using |b - A*x| instead of via the updated r vector.
-function [x, residnorm, residnorm2] = SD(A, b, x, nmax)
+function x = SD(A, b, x, nmax)
   Ad = zeros(size(x));
   r = b - A*x;
   alpha = 0;
   for n = 1:nmax
     r = r - alpha * Ad;
-    residnorm(n) = norm(r);
-    residnorm2(n) = norm(b - A*x);
     d = r;
     Ad = A*d;
     alpha = d' * r / (d' * Ad);
--- benchmul.m
+++ benchmul.m
@@ -1,22 +1,17 @@
-ms = round(logspace(1, 3, 50));
-gflops = zeros(1,length(ms));
-for i = 1:length(ms)
-  m = ms(i);
+gflops = zeros(1,1000);
+for m = 1:1000
   A = rand(m,m);
   B = rand(m,m);
   t = 0;
   iters = 1;
   % run benchmark for at least 0.1 seconds
   while (t < 0.1)
-    tic
+    t = cputime;
     for iter = 1:iters
       C = A * B;
     end
-    t = toc; % elapsed time in seconds
+    t = cputime - t; % elapsed time in seconds
     iters = iters * 2;
   end
-  gflops(i) = 2*m^3 * 1e-9 / (t * 2/iters);
-  disp(sprintf('gflops for m=%d = %g after %d iters',m,gflops(i),iters/2));
-  drawnow update;
+  gflops(m) = 2*m^3 * 1e-9 / (t * 2/iters);
 end
-plot(ms, gflops, 'r-')
