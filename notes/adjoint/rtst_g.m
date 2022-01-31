06f1be014a5d169acc68197941ac6f6d7c06ddc4
ajhPHROS <phros23@gmail.com>
2022-01-31 10:25:03 -0500

    Clean psets and notes

--- notes/adjoint/rtst_g.m
+++
@@ -1,5 +0,0 @@
-% test for adjoint gradient of recurrence, in recurrence2.lyx:
-% computation of g^N, given p of length N
-function g = rtst_g(p)
-  x = rtst_x(p);
-  g = x(2)^2;
--- notes/adjoint/rtst_x.m
+++
@@ -1,9 +0,0 @@
-% test for adjoint gradient of recurrence, in recurrence2.lyx:
-% computation of x^N, given p of length N
-function x = rtst_x(p)
-  x = [1;0];
-  t = 0.1;
-  A = [cos(t) sin(t); -sin(t) cos(t)];
-  for i = 1:length(p)
-    x = A*x + [0;p(i)];
-  end
